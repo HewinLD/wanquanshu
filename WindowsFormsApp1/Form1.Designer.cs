257de8e270636ae273133a6aa13d7a3981f8c181
雷顿ix <36781453+HewinLD@users.noreply.github.com>
2023-10-04 14:47:36 +0800

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        string 字符 = "完全数有：";
+        int 数字;
+        void 方法()
+        {
+            for (int i = 1; true; i++)
+            {
+                int s = 0;
+                for (int t = i - 1; t >= 1; t--)
+                {
+                    if (i % t == 0) s += t;
+                }
+                if (s == i) 字符 += i.ToString() + " ";
+                数字 = i;
+            }
+        }
+        Thread 线程;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            线程 = new Thread(方法);
+            线程.Start();
+            timer1.Enabled = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled != true)
+            {
+                timer1.Enabled = true;
+                线程.Resume();
+            }
+            else
+            {
+                timer1.Enabled = false;
+                线程.Suspend();
+                button1.Text = 字符 + "  (已暂停)";
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.Text = 字符;
+            label1.Text = 数字.ToString();
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1194, 610);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "找完全数";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 614);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 675);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "寻找完全数";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.Button button1;
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
